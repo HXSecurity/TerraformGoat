5187f1300d3b846861385bc1726026b51b5d8129
TeamsSix <49087564+teamssix@users.noreply.github.com>
2022-05-19 14:48:52 +0800

    feat: add aliyun oss object public access scenario

--- aliyun/oss/object_public_access/main.tf
+++ aliyun/oss/object_public_access/main.tf
@@ -9,6 +9,14 @@ resource "alicloud_oss_bucket" "huoxian_terraformgoat_bucket" {
   bucket        = "huoxian-terraformgoat-bucket-${random_string.random_suffix.result}"
 }
 
+resource "alicloud_oss_bucket_object" "huoxian_terraformgoat_object" {
+  bucket     = alicloud_oss_bucket.huoxian_terraformgoat_bucket.bucket
+  acl        = "public-read"
+  key        = "flag"
+  source     = "./flag"
+  depends_on = [alicloud_oss_bucket.huoxian_terraformgoat_bucket]
+}
+
 resource "random_string" "random_suffix" {
   length  = 5
   special = false
